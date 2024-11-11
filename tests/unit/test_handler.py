b14d2a5bf01f734a6714c154186b387ac9c67511
silvhua <silvhua@gmail.com>
2024-11-10 21:29:28 -0800

    enable unit test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -11,7 +11,7 @@ def apigw_event():
     filepath = '../events/event.json'
 
     with open(filepath) as file:
-        event_body = json.load(file).get('body', '{ "test": "body"}')
+        event_body = json.load(file).get('body', '"filename": "receipt_costco.jpg"')
 
 
     return {
@@ -73,5 +73,5 @@ def test_lambda_handler(apigw_event):
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
+    assert "total_amount" in ret["body"]
     # assert data["message"] == "hello world"
